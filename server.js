62c9632cefd3f5fa1a3b8152aef1b8079d1e366e
bharathsekaran0712 <bharathsekaran0712@gmail.com>
2026-05-11 18:16:22 +0530

    server updated

--- server.js
+++ server.js
@@ -1,25 +1,57 @@
-const dotenv = require("dotenv")
-const path = require("path")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
 
-dotenv.config({
-  path: path.join(__dirname, "config", "config.env"),
-})
+// Routes
+const product = require("./routes/productRoutes")
+const user = require("./routes/userRoutes")
+const cartRoutes = require("./routes/cartRoutes")
+const order = require("./routes/orderRoutes")
+const address = require("./routes/addressRoutes")
+const paymentRoutes = require("./routes/paymentRoutes")
 
-const app = require("./app")
-const connectDB = require("./config/db")
+const app = express()
 
+// Middleware
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+)
+
+app.use(express.json())
+
+// Routes
+app.use("/api/v1", product)
+app.use("/api/v1", user)
+app.use("/api/", cartRoutes)
+app.use("/api/v1", order)
+app.use("/api/address", address)
+app.use("/api/payment", paymentRoutes)
+
+// MongoDB Connection + Server Start
 const startServer = async () => {
   try {
 
-    await connectDB()
+    const data = await mongoose.connect("mongodb+srv://bharath:bharath123@clustor0.cxtmv2k.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=ecommerce")
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is listening to PORT ${process.env.PORT}`)
+    console.log(
+      "Mongoose is connected with server:",
+      data.connection.host
+    )
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is listening to PORT ${process.env.PORT || 8000}`
+      )
     })
 
-  } catch (error) {
+  } catch (err) {
 
-    console.log(error)
+    console.log(err.message)
+    process.exit(1)
 
   }
 }
